07ebe94eff0ba06eb410d0efc5617f7e8a39e76f
m-ji97 <tv5035@naver.com>
2024-01-04 10:05:29 +0900

    24-01-04 데이터 수정

--- src/com/javaex/ex05/Song.java
+++ src/com/javaex/ex05/Song.java
@@ -1,57 +1,67 @@
 package com.javaex.ex05;
 
 public class Song {
-	
-	//필드
-	public String title;
-	public String artist;
-	public String album;
-	public String compuser;
-	public String year;
-	public String track;
-	
-	//생성자
-	//메소드-g/s
-	public void setTitle(String title) {
-		this.title=title;
-	}
-	public void setArtist(String artist) {
-		this.artist=artist;
-	}
-	public void setAlbum(String album) {
-		this.album=album;
-	}
-	public void setCompuser(String compuser) {
-		this.compuser=compuser;
-	}
-	public void setYear(String year) {
-		this.year=year;
-	}
-	public void setTrack(String track) {
-		this.track=track;
-	}
+
+	// 필드
+	private String title;
+	private String artist;
+	private String album;
+	private String compuser;
+	private int year;
+	private int track;
+
+	// 생성자
+	// 메소드-g/s
 	public String getTitle() {
-		return this.title;
-	}
-	public String getArtist() {
-		return this.artist;
-	}
-	public String getAlbum() {
-		return this.album;
-	}
-	public String getCompuser() {
-		return this.compuser;
-	}
-	public String getYear() {
-		return this.year;
-	}
-	public String getTrack() {
-		return this.track;
-	}
-	
-	//메소드-일반
-	public void showinfo() {
-		System.out.println(this.artist+", "+this.title+"("+this.album+", "+this.year+", "+this.track+" track, "+this.compuser+" 작곡 )");
+		return title;
 	}
 
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getArtist() {
+		return artist;
+	}
+
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+
+	public String getAlbum() {
+		return album;
+	}
+
+	public void setAlbum(String album) {
+		this.album = album;
+	}
+
+	public String getCompuser() {
+		return compuser;
+	}
+
+	public void setCompuser(String compuser) {
+		this.compuser = compuser;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public int getTrack() {
+		return track;
+	}
+
+	public void setTrack(int track) {
+		this.track = track;
+	}
+
+	// 메소드-일반
+	public void showinfo() {
+		System.out.println(this.artist+", "+this.title+"("+this.album+", "+this.year+", "+this.track+"번 track, "+this.compuser+" 작곡 )");
+	}
 }
--- src/com/javaex/ex05/SongApp.java
+++ src/com/javaex/ex05/SongApp.java
@@ -6,24 +6,24 @@ public class SongApp {
 		s1.setAlbum("Real");
 		s1.setArtist("아이유");
 		s1.setTitle("좋은날");
-		s1.setYear("2010");
-		s1.setTrack("3번");
+		s1.setYear(2010);
+		s1.setTrack(3);
 		s1.setCompuser("이민수");
 		
 		Song s2 = new Song();
 		s2.setAlbum("Always");
 		s2.setArtist("BIGBANG");
 		s2.setTitle("거짓말");
-		s2.setYear("2007");
-		s2.setTrack("2번");
+		s2.setYear(2007);
+		s2.setTrack(2);
 		s2.setCompuser("G-DRAGON");
 		
 		Song s3 = new Song();
 		s3.setAlbum("버스커버스커1집");
 		s3.setArtist("버스커버스커");
 		s3.setTitle("벚꽃엔딩");
-		s3.setYear("2012");
-		s3.setTrack("4번");
+		s3.setYear(2012);
+		s3.setTrack(4);
 		s3.setCompuser("장범준");
 		
 		s1.showinfo();
